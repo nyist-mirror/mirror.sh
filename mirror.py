16034f006abab640cf2bb930d21ca65652125896
jelly <zhishuangnian@foxmail.com>
2017-05-28 22:13:45 +0800

    例行提交

--- mirror.py
+++ mirror.py
@@ -1,47 +1,89 @@
-import yaml
 from optparse import OptionParser
-from mako.template import Template
+#from mako.template import Template
+import logging
+import json
+import time
 
+logging.basicConfig(level=logging.INFO,format="%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s",datefmt='%F  %H:%M:%S')#,filename=os.path.join(dirname,"log/dbbackup.log"),filemode='a')
 
 parser = OptionParser(usage="%prog [options]")
-parser.add_option("-c", "--config",
+parser.add_option("--name",
                   action="store",
                   type="string",
-                  dest="config",
-                  help="Specify configuration file path")
-parser.add_option('-t',
-               action="store_true",
-               dest="test",
-               default=False,
-               help="Test configuration file format")
+                  dest="name",
+                  help="mirror name")
 
+parser.add_option('--status',
+               action="store",
+               dest="status",
+               type="string",
+               help="exit code")
+
+parser.add_option('--size',
+               action="store",
+               dest="size",
+               type="string",
+               help="dictory size")
+
+parser.add_option('--num',
+               action="store",
+               dest="num",
+               type="int",
+               help="file num")
+
+parser.add_option("--db",
+                  action="store",
+                  type="string",
+                  dest="db",
+                  help="db name")
 (options, args) = parser.parse_args()
 
-if not options.config:
-    print('error: Configuration file is required, use the -c parameter to specify the configuration file path')
+if not options.name:
+    logging.error('error: Mirror name is required, use the --name parameter to specify the mirror name')
+    raise Exception('error: Mirror name is required, use the --name parameter to specify the mirror name')
     exit(-1)
 
-if options.test:
-    with open(options.config) as f:
-        try:
-            x = yaml.load(f)
-        except Exception,e:
-            print("test config file %s failed" % options.config)
-            print(e)
-            exit(-1)
-        else:
-            print("test config file %s success"%options.config)
-            exit(0)
+if not options.status:
+    logging.error('error: Last task exit code is required, use the --status parameter to specify the exit code')
+    raise Exception('error: Last task exit code is required, use the --status parameter to specify the exit code')
+    exit(-1)
+
+if not options.num:
+    logging.error('error: Dictory file sum number  is required, use the --num parameter to specify it')
+    raise Exception('error: Dictory file sum number  is required, use the --num parameter to specify it')
+    exit(-1)
+
+if not options.size:
+    logging.error('error: Dictory file size  is required, use the --size parameter to specify it')
+    raise Exception('error: Dictory file size  is required, use the --size parameter to specify it')
+    exit(-1)
+
+if not options.db:
+    logging.error('error: Db file size  is required, use the --db parameter to specify it')
+    raise Exception('error: Db file size  is required, use the --size parameter to specify it')
+    exit(-1)
+
+with open(options.db) as db:
+    database = db.read()
+
+try:
+    data=json.loads(database)
+except ValueError:
+    logging.warning("the database is empty...")
+    data={}
 
 
-with open(options.config) as f:
-    config = yaml.load(f)
-    zhtemplate = Template(filename=config['templete']['templateFile_zh_CN'],
+data[options.name]={'status':options.status,'num':options.num,'size':options.size,'time':str(time.strftime('%Y-%m-%d %H:%M:%S'))}
+
+logging.info(data)
+
+with open(options.db,'w') as db:
+    db.write(json.dumps(data))
+
+
+zhtemplate = Template(filename='templates/index.html',
                           input_encoding='utf-8',
                           output_encoding='utf-8',
                           encoding_errors='replace')
-    #print config['template']
-    print zhtemplate.render(**config['templete'])
 
-
-
+print zhtemplate.render(data)
